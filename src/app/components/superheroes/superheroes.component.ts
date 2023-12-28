7cc781b9a71fae7b6e44eda5faaf4efc34d6e3c6
Karem <karem@gmail.com>
2023-12-28 18:44:55 -0500

    Incluir botones de modificar y eliminar angular material

--- src/app/components/superheroes/superheroes.component.ts
+++ src/app/components/superheroes/superheroes.component.ts
@@ -2,13 +2,16 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Superheroe } from '../../models/superheroe.model';
 import { RouterLink } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
 @Component({
   selector: 'app-superheroes',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './superheroes.component.html',
   styleUrl: './superheroes.component.css'
 })
