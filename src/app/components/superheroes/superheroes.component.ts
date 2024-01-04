e4d2990a5e14e2c57b23bd1b83fdb2a639deb947
Karem <karem@gmail.com>
2024-01-04 06:09:33 -0500

    Eliminar off

--- src/app/components/superheroes/superheroes.component.ts
+++ src/app/components/superheroes/superheroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Superheroe } from '../../models/superheroe.model';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,6 +14,7 @@ import {
 import { MatListModule } from '@angular/material/list';
 import { Observable } from 'rxjs';
 import { ServiceSHService } from '../../services/service-sh.service';
+import { Router } from 'express';
 
 
 
@@ -27,21 +28,22 @@ import { ServiceSHService } from '../../services/service-sh.service';
   styleUrl: './superheroes.component.css'
 })
 export class SuperheroesComponent implements OnInit {
- 
-  
+
+  sh: Superheroe[] = [];
 
   @Input() Superheroe: Superheroe = new Superheroe(0, '', '', '', '', '', '');
   @Input() indice: number = 0;
 
-  constructor(private servicioSH: ServiceSHService) { }
+  constructor(private servicioSH: ServiceSHService ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
 
-  EliminarSH(): void {
+  }
+  eliminar(): void {
     let obs$ = new Observable<Superheroe>;
-    obs$ = this.servicioSH.EliminarSH(this.indice);
-    obs$.subscribe(data=>{ this.Superheroe= data });  
-   }
-
+    obs$ = this.servicioSH.eliminarSH(this.indice);
+    obs$.subscribe(data=>{this.Superheroe=data});
+    alert(this.indice);
+  }
 }
 
--- src/app/services/service-sh.service.ts
+++ src/app/services/service-sh.service.ts
@@ -4,60 +4,68 @@ import { Observable } from 'rxjs';
 import { Superheroe } from '../models/superheroe.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ServiceSHService {
 
-  url: string = "http://localhost:3000/Superheroes";
+    url: string = "http://localhost:3000/Superheroes";
 
-  sh: Superheroe[] = [];
+    sh: Superheroe[] = [];
 
-  constructor(private http: HttpClient) { }
+    superheroe: Superheroe = new Superheroe(0, '', '', '', '', '', '');
 
-  getDatosSH(): Observable<Superheroe[]> {
-    let obs$ = this.http.get<Superheroe[]>(this.url);
-    obs$.subscribe(data => { this.sh = data });
-    return obs$;
-  }
-  //==========================================================================================================
-  //INSERTAR UN SUPERHEROE
-  //==========================================================================================================
-  postSuperHeroe(shNew: Superheroe): void {
+    constructor(private http: HttpClient) { }
 
-    this.http.post<Superheroe>(this.url,
-      {
-        'id': 0,
-        'superheroe': shNew.superheroe,
-        'editor': shNew.editor,
-        'actor_principal': shNew.actor_principal,
-        'tematica': shNew.tematica,
-        'imagen': shNew.imagen,
-        'personajes': shNew.personajes
-      }).subscribe(data => { shNew = data });
+    getDatosSH(): Observable<Superheroe[]> {
+        let obs$ = this.http.get<Superheroe[]>(this.url);
+        obs$.subscribe(data => { this.sh = data });
+        return obs$;
+    }
+    //==========================================================================================================
+    //INSERTAR UN SUPERHEROE
+    //==========================================================================================================
+    postSuperHeroe(shNew: Superheroe): void {
 
-
-  }
-
-
-  //==========================================================================================================
-  //ELIMINAR UN SUPERHEROE
-  //==========================================================================================================
-  EliminarSH(index: number): Observable<Superheroe> {
-    let id: number = Number(index);
-    let urlMod = this.url + id;
-    return this.http.delete<Superheroe>(urlMod);
-  }
+        this.http.post<Superheroe>(this.url,
+            {
+                'id': 0,
+                'superheroe': shNew.superheroe,
+                'editor': shNew.editor,
+                'actor_principal': shNew.actor_principal,
+                'tematica': shNew.tematica,
+                'imagen': shNew.imagen,
+                'personajes': shNew.personajes
+            }).subscribe(data => { shNew = data });
 
 
-  
-  // ModificarSH(): void {
-  //   this.http.put<Superheroe>(this.url,
-  //     {
-  //       "id": 3,
-  //       "titulo": "ANTONIO Y CLEOPATRA (nueva edición) ",
-  //       "tematica": "Drama"
-  //     }).subscribe(data => { this.= data; });
-  // }
+    }
+
+
+    //==========================================================================================================
+    //ELIMINAR UN SUPERHEROE
+    //==========================================================================================================
+
+
+    eliminarSH(index: number): Observable<Superheroe> {
+        let id: number= Number (index);
+        let miurl= this.url+ id;
+        return this.http.delete<Superheroe>(miurl);
+    }
+
+
+    borrar(sh:Superheroe){
+        const urls = `http://localhost:3000/Superheroes/${sh.id}`;
+        this.http.delete(urls);
+    }
+
+    // ModificarSH(): void {
+    //   this.http.put<Superheroe>(this.url,
+    //     {
+    //       "id": 3,
+    //       "titulo": "ANTONIO Y CLEOPATRA (nueva edición) ",
+    //       "tematica": "Drama"
+    //     }).subscribe(data => { this.= data; });
+    // }
 
 
 }
