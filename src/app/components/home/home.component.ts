e5bebac101ca4e3d21ae25eddcdb6fab0cc2f143
Karem <karem@gmail.com>
2024-01-07 17:29:48 -0500

    INSTALACION DE NGX-SPINNER

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
+import { NgxSpinner, NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, LoginComponent, NgxSpinnerModule, BrowserAnimationsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
